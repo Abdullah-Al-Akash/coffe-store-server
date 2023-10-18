e75416f7b7b30c399ebe3ca66f181d852b6aee00
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-10-18 14:58:29 +0600

    Load All Coffee

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,6 +13,53 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Coffee Making Server is Running!');
 })
+// MongoDB Connection Setup:
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tnc913c.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Create Database and Collection:
+        const coffeeCollection = client.db('coffeeHouse').collection('coffee');
+
+        // Add New Coffee:
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+        // Load All Coffee:
+        app.get('/coffee', async (req, res) => {
+            const cursor = coffeeCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
+
 
 app.listen(port, (req, res) => {
     console.log(`Coffee Server Running on: ${port}`);
